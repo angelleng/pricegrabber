5c2156bfe40a1f47c1aea06660d469431f220a23
Jing <angellj123@gmail.com>
2015-06-16 21:24:33 -0400

    should be working

---
+++ flightsearch.R
@@ -0,0 +1,32 @@
+a <- "http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&ar.rt.leaveSlice.orig.key=DTW&_ar.rt.leaveSlice.originRadius=0&ar.rt.leaveSlice.dest.key=WUH&_ar.rt.leaveSlice.destinationRadius=0&ar.rt.leaveSlice.date=7%2F1%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=8%2F1%2F15&ar.rt.returnSlice.time=Anytime&_ar.rt.flexAirSearch=0&ar.rt.numAdult=1&ar.rt.numSenior=0&ar.rt.numChild=0&ar.rt.child%5B0%5D=&ar.rt.child%5B1%5D=&ar.rt.child%5B2%5D=&ar.rt.child%5B3%5D=&ar.rt.child%5B4%5D=&ar.rt.child%5B5%5D=&ar.rt.child%5B6%5D=&ar.rt.child%5B7%5D=&search=Search+Flights&_ar.rt.nonStop=0&_ar.rt.narrowSel=0&ar.rt.narrow=airlines&ar.rt.carriers%5B0%5D=&ar.rt.carriers%5B1%5D=&ar.rt.carriers%5B2%5D=&ar.rt.cabin=C"
+
+b <- "http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&ar.rt.leaveSlice.orig.key=DTW&_ar.rt.leaveSlice.originRadius=0&ar.rt.leaveSlice.dest.key=WUH&_ar.rt.leaveSlice.destinationRadius=0&ar.rt.leaveSlice.date=7%2F1%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=8%2F1%2F15&ar.rt.returnSlice.time=Anytime&_ar.rt.flexAirSearch=0&ar.rt.numAdult=1&ar.rt.numSenior=0&ar.rt.numChild=0&ar.rt.child%5B0%5D=&ar.rt.child%5B1%5D=&ar.rt.child%5B2%5D=&ar.rt.child%5B3%5D=&ar.rt.child%5B4%5D=&ar.rt.child%5B5%5D=&ar.rt.child%5B6%5D=&ar.rt.child%5B7%5D=&search=Search+Flights&_ar.rt.nonStop=0&_ar.rt.narrowSel=0&ar.rt.narrow=airlines&ar.rt.carriers%5B0%5D=&ar.rt.carriers%5B1%5D=&ar.rt.carriers%5B2%5D=&ar.rt.cabin=C"
+
+a == b
+diff(a,b)
+
+leaveSlice.date=7%2F1%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=8%2F1%2F15&ar.rt.returnSlice.time=Anytime
+
+
+7%2F1%2F15&
+8%2F1%2F15&
+  
+
+departure_date <- as.Date("2015-07-01") + 1:40; departure_date
+return_date <- lapply(departure_date, function(i) i + 21:40)
+names(return_date) <- departure_date
+
+rt <- do.call("c", return_date)
+
+dp <- sort(rep(departure_date, 20))
+
+pairs <-  data.frame(departure = dp, returndate = rt)
+
+distinct <- cbind(format(pairs, "%m"),format(pairs, "%d"))
+names(distinct) <- c("d_month", "r_month", "d_day", "r_day")
+
+urls <- 
+paste0("http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&ar.rt.leaveSlice.orig.key=DTW&_ar.rt.leaveSlice.originRadius=0&ar.rt.leaveSlice.dest.key=WUH&_ar.rt.leaveSlice.destinationRadius=0&ar.rt.leaveSlice.date=", 
+       distinct$d_month, "%2F",distinct$d_day, "%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=", distinct$r_month, "%2F", distinct$r_day, "%2F15&ar.rt.returnSlice.time=Anytime&_ar.rt.flexAirSearch=0&ar.rt.numAdult=1&ar.rt.numSenior=0&ar.rt.numChild=0&ar.rt.child%5B0%5D=&ar.rt.child%5B1%5D=&ar.rt.child%5B2%5D=&ar.rt.child%5B3%5D=&ar.rt.child%5B4%5D=&ar.rt.child%5B5%5D=&ar.rt.child%5B6%5D=&ar.rt.child%5B7%5D=&search=Search+Flights&_ar.rt.nonStop=0&_ar.rt.narrowSel=0&ar.rt.narrow=airlines&ar.rt.carriers%5B0%5D=&ar.rt.carriers%5B1%5D=&ar.rt.carriers%5B2%5D=&ar.rt.cabin=C")
+
+scripts <- paste("phantomjs", "waitoncondition.js", urls)
--- waitoncondition.js
+++ waitoncondition.js
@@ -32,9 +32,6 @@
 // };
 
 
-var page = require('webpage').create(),
-address = "http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&ar.rt.leaveSlice.orig.key=DTW&_ar.rt.leaveSlice.originRadius=0&ar.rt.leaveSlice.dest.key=WUH&_ar.rt.leaveSlice.destinationRadius=0&ar.rt.leaveSlice.date=6%2F28%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=7%2F28%2F15&ar.rt.returnSlice.time=Anytime&_ar.rt.flexAirSearch=0&ar.rt.numAdult=1&ar.rt.numSenior=0&ar.rt.numChild=0&ar.rt.child%5B0%5D=&ar.rt.child%5B1%5D=&ar.rt.child%5B2%5D=&ar.rt.child%5B3%5D=&ar.rt.child%5B4%5D=&ar.rt.child%5B5%5D=&ar.rt.child%5B6%5D=&ar.rt.child%5B7%5D=&search=Search+Flights&_ar.rt.nonStop=0&_ar.rt.narrowSel=0&ar.rt.narrow=airlines&ar.rt.carriers%5B0%5D=&ar.rt.carriers%5B1%5D=&ar.rt.carriers%5B2%5D=&ar.rt.cabin=C";
-
 // page.onResourceRequested = function (req) {
 //     console.log('requested: ' + JSON.stringify(req, undefined, 4));
 // };
@@ -42,12 +39,20 @@ address = "http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&
 // page.onResourceReceived = function (res) {
 //     console.log('received: ' + JSON.stringify(res, undefined, 4));
 // };
+// http://www.orbitz.com/shop/home?type=air&ar.type=roundTrip&strm=true&ar.rt.leaveSlice.orig.key=DTW&_ar.rt.leaveSlice.originRadius=0&ar.rt.leaveSlice.dest.key=WUH&_ar.rt.leaveSlice.destinationRadius=0&ar.rt.leaveSlice.date=7%2F1%2F15&ar.rt.leaveSlice.time=Anytime&ar.rt.returnSlice.date=8%2F1%2F15&ar.rt.returnSlice.time=Anytime&_ar.rt.flexAirSearch=0&ar.rt.numAdult=1&ar.rt.numSenior=0&ar.rt.numChild=0&ar.rt.child%5B0%5D=&ar.rt.child%5B1%5D=&ar.rt.child%5B2%5D=&ar.rt.child%5B3%5D=&ar.rt.child%5B4%5D=&ar.rt.child%5B5%5D=&ar.rt.child%5B6%5D=&ar.rt.child%5B7%5D=&search=Search+Flights&_ar.rt.nonStop=0&_ar.rt.narrowSel=0&ar.rt.narrow=airlines&ar.rt.carriers%5B0%5D=&ar.rt.carriers%5B1%5D=&ar.rt.carriers%5B2%5D=&ar.rt.cabin=C 
 
-page.open(
+var page = require('webpage').create(),
+system = require('system'); 
+
+if (system.args.length === 1) {
+    console.log('Try to pass some args when invoking this script!');
+} else {
+    address =  system.args[1].toString(); 
+
+    page.open(
     address, 
     function (status) {
         var hahaha = 1; 
-        console.log(new Date().getTime()); 
         if (status !== 'success') {
             console.log('FAIL to load the address');
         }
@@ -55,17 +60,16 @@ page.open(
             var interval = setInterval(function(){
                 hahaha = hahaha + 1; 
                 if (hahaha == 20) {
-
-                     var k = page.evaluate(function () {
-                        return document.getElementsByClassName("price")[0].childNodes[1].childNodes[1].innerHTML; 
+                    var k = page.evaluate(function () {
+                        if (document.getElementsByClassName("price").length > 0) {
+                        return [document.getElementsByClassName("price")[0].childNodes[1].childNodes[1].nodeValue, 
+                        document.getElementsByClassName("departDate")[0].innerHTML.trim(),
+                        document.getElementsByClassName("arriveDate")[0].innerHTML.trim()]; 
+                        } else return "Not available!!!"; 
                         });
-                    
-                    console.log("price is (2)" + k);
-                    var p = page.evaluate(function () {
-                        return document.getElementsByTagName('html')[0].innerHTML
-                    });
-                    console.log(p);
-                    
+                    console.log("Departure Date: " + k[1]); 
+                    console.log("Arrival Date: " + k[2])
+                    console.log("Price is: " + k[0]); 
                     phantom.exit();
                     clearInterval(interval); 
                 } 
@@ -90,3 +94,7 @@ page.open(
         } 
     }
     ); 
+}
+
+
+
